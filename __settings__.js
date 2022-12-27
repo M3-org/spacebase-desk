80d9a3a9196e7b717c6a9b20f9f2ea391a4d6b50
jin <32600939+madjin@users.noreply.github.com>
2022-12-26 21:40:56 -0500

    add files

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1550419.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 104677824, 104677780, 104677762, 104677763, 104677761, 104677714, 104677823, 104677848, 104677856, 104677782, 104677834, 104677836, 104677580, 104677837, 104678085, 104678083, 104678082, 104678100 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/104677800/1/basis.wasm.js', 'wasmUrl' : 'files/assets/104677801/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/104677805/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/104677799/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/104677790/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/104677809/1/ammo.js', 'preload' : true},
+];
